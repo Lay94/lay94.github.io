561e6c97fbda77a2fbb6ac44217d3204cd8358ed
Elaine G.C <33430866+Lay94@users.noreply.github.com>
2022-03-18 06:14:52 -0700

    Actualizado fichero

--- bd_dengue.sh
+++ bd_dengue.sh
@@ -1,13 +1,5 @@
 # Descarga y limpia la base de datos de Enfermedades Transmitidas por Vectores (Dengue)
 # Uso: bash bd_dengue.sh
-
-handle_error() {
-    echo "--La descarga de la base de datos falló, por favor, vuelva a intentarlo--"
-    exit 1
-}
-
-trap 'handle_error $LINENO $?' ERR
-
 rm -f *.csv #Eliminando bd para ser actulizada
 curl -L -O https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/etv/datos_abiertos_dengue.zip #Descargando bd
 unzip -q *.zip #Descomprimiendo el archivo
